ed1827ee3aa59bdd7f233eceee35f76ba8c48213
wuzb19951013 <574145129@qq.com>
2021-06-13 15:47:04 +0800

    可变数组与数组

--- array.m
+++ array.m
@@ -12,7 +12,8 @@ int main(int argc, const char * argv[]) {
         // insert code here...
 //        double distance[5];
 //        BOOL arrayBOOL[]={YES,NO,YES,YES,NO};
-        
+
+        //数组
         NSArray *array =[[NSArray alloc] initWithObjects:@"apple", @"banana",@"pear",nil];
         NSArray *fruit1 =[NSArray arrayWithObjects:@"apple", @"banana",@"pear",nil];
         NSArray *fruit2 =[[NSArray alloc] initWithArray:fruit1];//深拷贝
@@ -37,15 +38,35 @@ int main(int argc, const char * argv[]) {
         [fruit5 enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop){
             NSLog(@"%zi-->%@",idx,obj);
         }];//反向枚举 输出2-->pear 1-->banana 0-->apple
-        
-        //字符串转数组
+
+//        字符串转数组
         NSString *animalstr=@"cat,dog,horse,tiger";
         NSArray *animal=[animalstr componentsSeparatedByString:@","];//封装的分词方法。
         NSLog(@"animals=%@",animal);
-        
+
         NSString *animalstr2=[animal componentsJoinedByString:@">"];//用>连接
         NSLog(@"%@",animalstr2);
         
+        //可变数组，与数组内存有区别，可附加
+        NSMutableArray *mfruit=[[NSMutableArray alloc] init];
+        [mfruit addObject:@"apple"];
+        [mfruit addObjectsFromArray:@[@"banana",@"pear"]];// 加元素，用数组
+        NSLog(@"%@",mfruit);
+        
+        [mfruit insertObject:@"orange" atIndex:1];//插入到第二个位置
+        NSLog(@"%@",mfruit);
+        
+        NSMutableArray *mfruit2=[NSMutableArray arrayWithArray:mfruit];
+        [mfruit2 replaceObjectAtIndex:0 withObject:@"grape"];
+        [mfruit2 removeObject:@"pear"];//删除pear
+        [mfruit2 removeObjectAtIndex:1];//删除第2个
+        //此外还有removeLastObject,removeAllObject,
+        NSLog(@"%@",mfruit2);
+        
+        [mfruit removeObjectsInArray:@[@"banana",@"watermelon"]];
+        NSLog(@"%@",mfruit);//指删除有的，没有的元素不管
+        [mfruit exchangeObjectAtIndex:0 withObjectAtIndex:1];//交换两个元素
+        NSLog(@"%@",mfruit);
     }
     return 0;
 }
